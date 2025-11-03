46aa68bf130f70e4c7f0f169e28f18a128ab09f0
haovn2411 <haovn2411@gmail.com>
2025-11-03 10:05:50 +0700

    update test controller

---
+++ ProjectArgoCD/Controllers/ATestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectArgoCD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ATestController : ControllerBase
+    {
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IActionResult Get()
+        {
+            return Ok("This is a test endpoint from ATestController.");
+        }
+    }
+}
